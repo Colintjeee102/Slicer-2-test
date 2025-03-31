649cbf1c388622b79e2799756e4d907bd7370827
Colintjeee102 <colin.otten@icloud.com>
2025-03-31 14:35:31 +0200

    Update siemens_writer.cpp

--- src/gcode/writers/siemens_writer.cpp
+++ src/gcode/writers/siemens_writer.cpp
@@ -9,18 +9,21 @@ namespace ORNL
     SiemensWriter::SiemensWriter(GcodeMeta meta, const QSharedPointer<SettingsBase>& sb) : WriterBase(meta, sb) {
         m_M64 = "M64";
         m_M65 = "M65";
+        m_first_layer_written = false;
     }
 
     QString SiemensWriter::writeInitialSetup(Distance minimum_x, Distance minimum_y, Distance maximum_x, Distance maximum_y, int num_layers)
     {
-        m_current_z = m_sb->setting< Distance >(Constants::PrinterSettings::Dimensions::kZOffset);
-        m_current_w = m_sb->setting< Distance >(Constants::PrinterSettings::Dimensions::kWMax);
+        m_current_z = m_sb->setting<Distance>(Constants::PrinterSettings::Dimensions::kZOffset);
+        m_current_w = m_sb->setting<Distance>(Constants::PrinterSettings::Dimensions::kWMax);
         m_current_rpm = 0;
         m_extruders_on[0] = false;
         m_first_print = true;
         m_first_travel = true;
+        m_first_layer_travel_written = false;
         m_layer_start = true;
         m_min_z = 0.0f;
+
         QString rv;
         if (m_sb->setting< int >(Constants::PrinterSettings::GCode::kEnableStartupCode))
         {
@@ -66,11 +69,15 @@ namespace ORNL
         return rv;
     }
 
-    QString SiemensWriter::writeBeforeLayer(float new_min_z,QSharedPointer<SettingsBase> sb)
+    QString SiemensWriter::writeBeforeLayer(float new_min_z, QSharedPointer<SettingsBase> sb)
     {
         m_spiral_layer = sb->setting<bool>(Constants::ProfileSettings::SpecialModes::kEnableSpiralize);
         m_layer_start = true;
         QString rv;
+
+        // If this is the first layer and we haven't written the first travel yet,
+        // we'll handle it in the first writeTravel call
+
         return rv;
     }
 
@@ -103,9 +110,7 @@ namespace ORNL
     {
         QString rv;
 
-        //If extruder is left on because of no end of path modifier, turn it off using Perimeter end of path G-Code
-        //This syntax doesn't currently issue extruder on/off commands because they are manually input to the
-        //start/end G-Code of the settings
+        // If extruder is left on because of no end of path modifier, turn it off
         if(m_extruders_on[0])
         {
             m_extruders_on[0] = false;
@@ -113,64 +118,89 @@ namespace ORNL
                 rv += m_sb->setting< QString >(Constants::ProfileSettings::GCode::kPerimeterEnd) % m_newline;
         }
 
-        Point new_start_location;
-
-        //Use updated start location if this is the first travel
+        // Special handling for first travel
         if (m_first_travel) {
-            Point first_print_location = target_location; // De echte start van de print
-            Point offset_start = first_print_location + Point(0, -150000, 0); // 150 mm links van de eerste print
+            Point first_print_location = target_location;
 
-            rv += m_G1 % writeCoordinates(offset_start) % " EM=0" % commentSpaceLine("MOVE TO START POSITION");
-            rv += m_G1 % writeCoordinates(first_print_location) % " EM=1" % commentSpaceLine("MOVE TO PRINT START");
+            // Calculate a safe approach point
+            Point offset_start = first_print_location + Point(0, -150000, 0); // 150mm to the left of first print point
+
+            // Move to the approach point first at a safe speed
+            rv += m_G1 % writeCoordinates(offset_start);
+
+            // Apply the correct feedrate for the approach move
+            Velocity approachSpeed = m_sb->setting<Velocity>(Constants::ProfileSettings::Travel::kSpeed);
+            rv += m_f % QString::number(approachSpeed.to(m_meta.m_velocity_unit));
+
+            rv += " EM=0" % commentSpaceLine("APPROACH TO PRINT START");
+
+            // Then move to the actual print start point
+            rv += m_G1 % writeCoordinates(first_print_location);
+            rv += m_f % QString::number(approachSpeed.to(m_meta.m_velocity_unit));
+            rv += " EM=1" % commentSpaceLine("MOVE TO PRINT START");
 
             m_first_travel = false;
-            return rv; // Zorgt ervoor dat er geen extra travel wordt gegenereerd
+            return rv;
         }
-        else
-            new_start_location = start_location;
 
-        Distance liftDist;
-        liftDist = m_sb->setting< Distance >(Constants::ProfileSettings::Travel::kLiftHeight);
+        // Normal travel handling for subsequent moves
+        Point new_start_location = start_location;
 
-        bool travel_lift_required = liftDist > 0;// && !m_first_travel; //do not write a lift on first travel
+        // Handle travel lift if needed
+        Distance liftDist = m_sb->setting<Distance>(Constants::ProfileSettings::Travel::kLiftHeight);
+        bool travel_lift_required = liftDist > 0;
 
-        //Don't lift for short travel moves
-        if(start_location.distance(target_location) < m_sb->setting< Distance >(Constants::ProfileSettings::Travel::kMinTravelForLift))
+        // Don't lift for short travel moves
+        if(start_location.distance(target_location) < m_sb->setting<Distance>(Constants::ProfileSettings::Travel::kMinTravelForLift))
         {
             travel_lift_required = false;
         }
 
-        //travel_lift vector in direction normal to the layer
-        //with length = lift height as defined in settings
+        // Get travel lift vector
         QVector3D travel_lift = getTravelLift();
 
-        //write the lift
+        // Write the lift if needed
         if (travel_lift_required && (lType == TravelLiftType::kBoth || lType == TravelLiftType::kLiftUpOnly))
         {
-            Point lift_destination = new_start_location + travel_lift; //lift destination is above start location
-            rv += m_G1 % writeCoordinates(lift_destination) % " EM=0" % commentSpaceLine("TRAVEL LIFT Z");
-            setFeedrate(m_sb->setting< Velocity >(Constants::PrinterSettings::MachineSpeed::kZSpeed));
+            Point lift_destination = new_start_location + travel_lift;
+            rv += m_G1 % writeCoordinates(lift_destination);
+
+            // Apply the correct feedrate for Z movement
+            Velocity zSpeed = m_sb->setting<Velocity>(Constants::PrinterSettings::MachineSpeed::kZSpeed);
+            rv += m_f % QString::number(zSpeed.to(m_meta.m_velocity_unit));
+
+            rv += " EM=0" % commentSpaceLine("TRAVEL LIFT Z");
+            setFeedrate(zSpeed);
         }
 
-        //write the travel
+        // Write the travel move
         Point travel_destination = target_location;
         if (travel_lift_required)
         {
-            travel_destination = travel_destination + travel_lift; //travel destination is above the target point
+            travel_destination = travel_destination + travel_lift;
         }
 
-        rv += m_G1 % writeCoordinates(travel_destination) % " EM=0" % commentSpaceLine("TRAVEL");
-        setFeedrate(m_sb->setting< Velocity >(Constants::ProfileSettings::Travel::kSpeed));
+        rv += m_G1 % writeCoordinates(travel_destination);
 
-        //write the travel lower (undo the lift)
+        // Apply the correct feedrate for XY travel
+        Velocity travelSpeed = m_sb->setting<Velocity>(Constants::ProfileSettings::Travel::kSpeed);
+        rv += m_f % QString::number(travelSpeed.to(m_meta.m_velocity_unit));
+
+        rv += " EM=0" % commentSpaceLine("TRAVEL");
+        setFeedrate(travelSpeed);
+
+        // Write the travel lower if needed
         if (travel_lift_required && (lType == TravelLiftType::kBoth || lType == TravelLiftType::kLiftLowerOnly))
         {
-            rv += m_G1 + writeCoordinates(target_location) % " EM=0" % commentSpaceLine("TRAVEL LOWER Z");
-            setFeedrate(m_sb->setting<Velocity>(Constants::PrinterSettings::MachineSpeed::kZSpeed));
-        }
+            rv += m_G1 + writeCoordinates(target_location);
 
-        if (m_first_travel) //if this is the first travel
-            m_first_travel = false; //update for next one
+            // Apply the correct feedrate for Z movement
+            Velocity zSpeed = m_sb->setting<Velocity>(Constants::PrinterSettings::MachineSpeed::kZSpeed);
+            rv += m_f % QString::number(zSpeed.to(m_meta.m_velocity_unit));
+
+            rv += " EM=0" % commentSpaceLine("TRAVEL LOWER Z");
+            setFeedrate(zSpeed);
+        }
 
         return rv;
     }
@@ -181,26 +211,9 @@ namespace ORNL
         int rpm = params->setting<int>(Constants::SegmentSettings::kExtruderSpeed);
         RegionType region_type = params->setting<RegionType>(Constants::SegmentSettings::kRegionType);
         PathModifiers path_modifiers = params->setting<PathModifiers>(Constants::SegmentSettings::kPathModifiers);
-        float output_rpm = rpm * m_sb->setting< float >(Constants::PrinterSettings::MachineSpeed::kGearRatio);
+        float output_rpm = rpm * m_sb->setting<float>(Constants::PrinterSettings::MachineSpeed::kGearRatio);
 
         QString rv;
-
-        //turn on the extruder if it isn't already on
-        if (m_extruders_on[0] == false && rpm > 0)
-        {
-            rv += writeExtruderOn(region_type, rpm);
-        }
-
-        if (rpm != m_current_rpm && rpm == 0)
-        {
-            rv += writeExtruderOff();
-            m_current_rpm = rpm;
-        }
-        else if (rpm != m_current_rpm)
-        {
-            rv += m_M3 % m_s % QString::number(output_rpm) % commentSpaceLine("UPDATE EXTRUDER RPM");
-            m_current_rpm = rpm;
-        }
 
         rv += m_G1;
         if (getFeedrate() != speed || m_layer_start)
@@ -210,7 +223,7 @@ namespace ORNL
             m_layer_start = false;
         }
 
-        //writes WXYZ to destination
+        // Writes WXYZ to destination
         if (path_modifiers == PathModifiers::kForwardTipWipe || path_modifiers == PathModifiers::kReverseTipWipe)
         {
             rv += writeCoordinates(target_point) % " EM=0";
@@ -220,7 +233,7 @@ namespace ORNL
             rv += writeCoordinates(target_point) % " EM=1";
         }
 
-        //add comment for gcode parser
+        // Add comment for gcode parser
         if (path_modifiers != PathModifiers::kNone)
             rv += commentSpaceLine(toString(region_type) % m_space % toString(path_modifiers));
         else
@@ -231,26 +244,26 @@ namespace ORNL
         return rv;
     }
 
-
     QString SiemensWriter::writeArc(const Point &start_point,
-                                           const Point &end_point,
-                                           const Point &center_point,
-                                           const Angle &angle,
-                                           const bool &ccw,
+                                    const Point &end_point,
+                                    const Point &center_point,
+                                    const Angle &angle,
+                                    const bool &ccw,
                                     const QSharedPointer<SettingsBase> params)
     {
         QString rv;
 
         Velocity speed = params->setting<Velocity>(Constants::SegmentSettings::kSpeed);
-        int rpm = params->setting<int>(Constants::SegmentSettings::kExtruderSpeed);
+        // Remove RPM-related code
+        // int rpm = params->setting<int>(Constants::SegmentSettings::kExtruderSpeed);
         int material_number = params->setting<int>(Constants::SegmentSettings::kMaterialNumber);
         auto region_type = params->setting<RegionType>(Constants::SegmentSettings::kRegionType);
         auto path_modifiers = params->setting<PathModifiers>(Constants::SegmentSettings::kPathModifiers);
 
-        // Turn on the extruder if it isn't already on
-        if (!m_extruders_on[0] && rpm > 0)
+        // Modify extruder on check to not depend on RPM
+        if (!m_extruders_on[0])
         {
-            rv += writeExtruderOn(region_type, rpm);
+            rv += writeExtruderOn(region_type, 0); // Pass 0 as RPM or modify writeExtruderOn to not use RPM
         }
 
         rv += ((ccw) ? m_G3 : m_G2);
@@ -260,18 +273,20 @@ namespace ORNL
             setFeedrate(speed);
             rv += m_f % QString::number(speed.to(m_meta.m_velocity_unit));
         }
-        if (rpm != m_current_rpm)
-        {
-            rv += m_s % QString::number(rpm);
-            m_current_rpm = rpm;
-        }
+
+        // Remove RPM setting code
+        // if (rpm != m_current_rpm)
+        // {
+        //     rv += m_s % QString::number(rpm);
+        //     m_current_rpm = rpm;
+        // }
 
         rv += m_i % QString::number(Distance(center_point.x() - start_point.x()).to(m_meta.m_distance_unit), 'f', 4) %
               m_j % QString::number(Distance(center_point.y() - start_point.y()).to(m_meta.m_distance_unit), 'f', 4) %
               m_x % QString::number(Distance(end_point.x()).to(m_meta.m_distance_unit), 'f', 4) %
               m_y % QString::number(Distance(end_point.y()).to(m_meta.m_distance_unit), 'f', 4) % " EM=1";
-        // write vertical coordinate along the correct axis (Z or W) according to printer settings
-        // only output Z/W coordinate if there was a change in Z/W
+
+        // Rest of the method remains unchanged
         Distance z_offset = m_sb->setting< Distance >(Constants::PrinterSettings::Dimensions::kZOffset);
 
         Distance target_z = end_point.z() + z_offset;
@@ -372,6 +387,12 @@ namespace ORNL
     QString SiemensWriter::writeExtruderOn(RegionType type, int rpm)
     {
         QString rv;
+        // Remove any code that uses the rpm parameter
+        // Instead, use a fixed value or remove RPM-related commands entirely
+
+        // Example: Set extruder on without specifying RPM
+        m_extruders_on[0] = true;
+
         return rv;
     }
 
@@ -404,3 +425,4 @@ namespace ORNL
     }
 
 }  // namespace Colin Otten
+
